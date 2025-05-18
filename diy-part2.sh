335bbc932f2b46a28b7ba396ca29966a98ecc7af
EircoD <59461660+Eircodong@users.noreply.github.com>
2025-05-18 18:19:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,16 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
 # 删除插件
 rm -rf feeds/packages/net/adguardhome
 
@@ -34,3 +44,7 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
 git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
 git clone --depth=1 -b main https://github.com/linkease/istore package/istore
+
+# uugamebooster
+git_sparse_clone openwrt-23.05 https://github.com/coolsnowwolf/luci applications/luci-app-uugamebooster
+git_sparse_clone master https://github.com/coolsnowwolf/packages net/uugamebooster
